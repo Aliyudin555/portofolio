3250a65f327ee0ac3ae62b0062a57914e7d4c6af
Aliyudin555 <aliyudinda146@gmail.com>
2025-02-05 12:08:18 +0700

    pembaruan

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -14,7 +14,7 @@ const about = () => {
 I have a strong foundation in front-end technologies such as HTML, CSS, JavaScript, and Bootstrap, as well as back-end using MySQL, PHP, and Laravel. With this combination, I am able to build functional, responsive, and user-friendly web solutions.
 I am interested in joining an innovative and collaborative team, where I can contribute optimally while continuing to learn to develop my abilities.
             </p>
-            <a href='https://drive.google.com/file/d/1kKwf-tPpCtWoQymzBNRCUzxVRLTBh8IO/view?usp=drive_link' className='btn'>Dwonload CV</a>
+            <a href='https://drive.google.com/file/d/13AGjItsmIDyOsnyfIMqyaLqqY-QtVwQj/view?usp=sharing' target="_blank" className='btn'>Dwonload CV</a>
           </div>
         </div>
       </div>
